b8927aa5e7a44559c3fd1dc8112ec0623a2b680e
alphonsa <alphonsanew22@gmail.com>
2023-06-13 12:12:44 +0530

    initial package commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Alphonsa Varghese on 13/06/23.
+//
+
+import Foundation
+
+public struct SDK {
+   public static func doSomeWork() {
+        print("Doing some work")
+    }
+}
